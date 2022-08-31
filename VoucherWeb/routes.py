42677d941e2d37ba129ad3abc65a6b839979618a
Flávio Escobar <flavioescobar93@gmail.com>
2022-08-31 16:18:20 -0300

    Create routes.py

---
+++ VoucherWeb/routes.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from VoucherWeb import app, database
+from VoucherWeb.forms import FormCriarConta, FormLogin, FormSolicitarVoucher
+from VoucherWeb.models import Usuario
+
+lista_usuarios = ['admin', 'usuario']
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    form_solicitarvoucher = FormSolicitarVoucher()
+    if form_solicitarvoucher.validate_on_submit and 'botao_submit_solicitarvoucher' in request.form:
+        flash("Este é o seu Voucher: {}".format('AA34DF53'),'alert-success')
+        return redirect(url_for('home'))
+    return render_template('home.html', form_solicitarvoucher=form_solicitarvoucher)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    if form_login.validate_on_submit() and 'botao_submit_fazerlogin' in request.form:
+        flash("Bem-Vindo {}".format(form_login.username.data), 'alert-success')
+        return redirect(url_for('home'))
+        #fez login com sucesso
+    return render_template('login.html', form_login=form_login)
+
+@app.route('/criar_conta', methods=['GET', 'POST'])
+def criar_conta():
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=form_criarconta.senha.data)
+        database.session.add(usuario)
+        database.session.commit()
+        flash("Conta Criada com Sucesso para: {}".format(form_criarconta.username.data),'alert-success')
+        return redirect(url_for('home'))
+        #Criou Conta com sucesso
+    return render_template('criar_conta.html', form_criarconta=form_criarconta)
