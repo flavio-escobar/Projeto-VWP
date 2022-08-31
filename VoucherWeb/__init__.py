8af4dd7f113c4d1df1bedc10addd0608308308ee
Flávio Escobar <flavioescobar93@gmail.com>
2022-08-31 17:38:34 -0300

    Update __init__.py

--- VoucherWeb/__init__.py
+++ VoucherWeb/__init__.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-lista_usuarios = ['admin', 'usuario']
-
 app.config['SECRET_KEY'] = '8b7250cf7c5dc00f81c393043ae04db7e19d08a376b835b0f5a2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///voucher.db'
 
